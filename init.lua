26b211d9c6386f1686dfacb8034fd2cd92ded94e
Jamie Pond <jamiepond259@gmail.com>
2023-02-12 23:28:42 +0000

    faff around and actually install

--- init.lua
+++ init.lua
@@ -14,6 +14,8 @@ require('packer').startup(function(use)
   -- Package manager
   use 'wbthomason/packer.nvim'
 
+  use 'github/copilot.vim'
+
   use { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     requires = {
@@ -56,10 +58,8 @@ require('packer').startup(function(use)
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
-
-  use 'github/copilot.vim' -- Install Copilot :) 
-
   -- Fuzzy Finder (files, lsp, etc)
+
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
