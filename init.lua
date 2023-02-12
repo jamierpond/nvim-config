b8da8aee60f296e227e6cb07abe7c274897c7a83
Jamie Pond <jamiepond259@gmail.com>
2023-02-12 23:17:53 +0000

    install copilot

--- init.lua
+++ init.lua
@@ -57,6 +57,8 @@ require('packer').startup(function(use)
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
 
+  use 'github/copilot.vim' -- Install Copilot :) 
+
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
